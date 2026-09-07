766c23d1cb75f7a255b65e2560fdf23bbe8c5f2b
sulagnaaaa <sulagnachowdhury2015@gmail.com>
2026-09-07 11:19:40 +0530

    cors changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const productRoutes = require("./src/routes/product.routes")
 
 const app = express()
 app.use(cors({
-    origin: ["product-management-frontend-snowy.vercel.app", "http://localhost:5173"],
+    origin: ["https://product-management-frontend-snowy.vercel.app/", "http://localhost:5173"],
     credentials: true
 }))
 connectDB();
